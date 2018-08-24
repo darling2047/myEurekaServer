877e3be2c9f084cb43bc1ac9cae823c79d8239e4
darling2047 <1647163987@qq.com>
2018-08-24 10:16:53 +0800

    222

---
+++ src/main/java/com/eureka/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.eureka.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer  // 声明此服务是eureka的服务
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
